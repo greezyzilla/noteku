f9efa389eddce6de6cab964c1f9af18c044160d3
Gunawan Wahyu <greezyzilla@gmail.com>
2022-11-08 15:00:28 +0700

    fix: :adhesive_bandage: fix toast when logout

--- components/organisms/header/index.tsx
+++ components/organisms/header/index.tsx
@@ -1,11 +1,22 @@
-import { useAppDispatch } from '../../../app/hooks';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { signOut } from '../../../features/auth';
 import { ToggleDarkMode, ToggleLocale } from '../../molecules';
 import Dropdown from '../../molecules/dropdown';
 import SearchNote from './searchNote';
 
 export default function Header() {
+  const locale = useAppSelector((state) => state.theme.locale);
   const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const onLogout = async () => {
+    dispatch(signOut());
+    if (locale === 'en') toast.success('Logged out successfully');
+    else toast.success('Berhasil keluar');
+    router.replace('/auth/login');
+  };
 
   return (
     <header className="flex h-fit w-full bg-white/80 shadow-sm dark:bg-slate-900/80">
@@ -23,7 +34,7 @@ export default function Header() {
             </div>
             <ToggleLocale />
             <ToggleDarkMode />
-            <Dropdown user={{ name: 'Gunawan' }} onLogout={() => dispatch(signOut())} />
+            <Dropdown user={{ name: 'Gunawan' }} onLogout={onLogout} />
           </div>
         </div>
       </div>
